f3727e6937c998c564ad157f377a40f393bb703a
Tom Taquet <ttaquet@c1r2p1.42lehavre.fr>
2025-08-05 17:53:59 +0200

    +~| Started loop, start new use handler and Updated class name

--- src/main/Irc.cpp
+++ src/main/Irc.cpp
@@ -2,53 +2,58 @@
 
 /* =========== Exception Handler =========== */
 
-const char* Irc::UnknownError::what() const throw() {return ("Unknown error.");}
+const char* Server::UnknownError::what() const throw() {return ("Unknown error.");}
 
-const char* Irc::ArgError::what() const throw() {return ("Argument of the constructor must be password and port.");}
+const char* Server::ArgError::what() const throw() {return ("Argument of the constructor must be password and port.");}
 
-const char* Irc::SocketError::what() const throw() {return ("The socket creation failed.");}
+const char* Server::SocketError::what() const throw() {return ("The socket creation failed.");}
 
-const char* Irc::BindError::what() const throw() {return ("Socket bind to server unsuccessful.");}
+const char* Server::BindError::what() const throw() {return ("Socket bind to server unsuccessful.");}
 
-const char* Irc::FcntlError::what() const throw() {return ("Socket is still in blocking mode.");}
+const char* Server::FcntlError::what() const throw() {return ("Socket is still in blocking mode.");}
 
-const char* Irc::ListenError::what() const throw() {return ("Server cant listen the socket.");}
+const char* Server::ListenError::what() const throw() {return ("Server cant listen the socket.");}
+
+const char* Server::SetsockoptError::what() const throw() {return ("Server can't set socket option.");}
+
+const char* Server::PollError::what() const throw() {return ("Poll failed to read the socket.");}
 
 /* ======= Constructor & Destructor ======= */
 
-Irc::Irc() {throw (ArgError());}
+Server::Server() {throw (ArgError());}
 
-Irc::Irc(std::string password, uint16_t port) :
+Server::Server(std::string password, uint16_t port) :
 _password(password), _port(port) {initSocket();}
 
-Irc::Irc(const Irc &other) {
+Server::Server(const Server &other) {
 	if (this != &other) {
 		_password = other._password;
 		_port = other._port;
 	}
 }
 
-Irc::~Irc() {}
+Server::~Server() {}
 
 /* ============ Private Function ============ */
 
-void Irc::initSocket() {
+void Server::initSocket() {
 	// Create the socket
 	// AF_INET -> IPv4
 	// SOCK_STREAM -> TCP
 	// 0 -> used protocol
-	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0)
+	_serverFd = socket(AF_INET, SOCK_STREAM, 0);
+	if (_serverFd < 0)
     	throw(SocketError());
 
-	if (fcntl(server_fd, F_SETFL, O_NONBLOCK) < 0)
+	if (fcntl(_serverFd, F_SETFL, O_NONBLOCK) < 0)
 		throw ((FcntlError()));
 
 	int opt = 1;
 	// allows the port to be reused without waiting the end of TIMEWAIT
 	// SOL_SOCKET -> level of the option (here is it for general option)
 	// SO_REUSEADDR -> Socket Re use Address
-	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	if (setsockopt(_serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+		throw (SetsockoptError());
 
 	// define the server's address
 	struct sockaddr_in address;
@@ -58,17 +63,43 @@ void Irc::initSocket() {
 	address.sin_port = htons(_port);
 
 	// bind the socket to the address
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	if (bind(_serverFd, (struct sockaddr *)&address, sizeof(address)) < 0)
 		throw (SocketError());
 
 	// SOMAXCONN -> Socket Max Connexion (default 1024)
-	if (listen(server_fd, SOMAXCONN) < 0)
+	if (listen(_serverFd, SOMAXCONN) < 0)
 		throw (ListenError());
+}
+
+void Server::newUser(std::vector<pollfd> fds) {
+	// Nouvelle connexion
+	struct sockaddr_in client_addr;
+	socklen_t addr_len = sizeof(client_addr);
+	int client_fd = accept(_serverFd, (struct sockaddr*)&client_addr, &addr_len);
+	if (client_fd >= 0) {
+		fds.push_back({client_fd, POLLIN, 0});
+		std::cout << "New client connected: fd = " << client_fd << std::endl;
+	}
+	else
+		std::cerr << "New client failed to connect" << std::endl;
 }
 
 /* ================= Loop ================= */
 
-void Irc::pollLoop() {
-	struct pollfd servFd;
-	servFd.fd = _port;
+void Server::pollLoop() {
+	std::vector<pollfd> fds;
+	fds.push_back({_serverFd, POLLIN, 0});  // Server to check with accept()
+
+	while (true) {
+		int activity = poll(fds.data(), fds.size(), -1); // -1 = block
+		if (activity < 0)
+			throw (PollError());
+
+		for (int i = 0; fds.size(); i++) {
+			if (fds[i].revents & POLLIN) {
+				if (fds[i].fd == _serverFd)
+					newUser(fds);
+			}
+		}
+	}
 }
