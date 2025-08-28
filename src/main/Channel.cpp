7dbeb92fae2073457ab8650c9d587a6acc808cc2
Hp de moa <taquettom3@gmail.com>
2025-08-28 12:48:07 +0200

    ~ | Upadted SendMessageToChannelUser to print command expect PRIVMSG

--- src/main/Channel.cpp
+++ src/main/Channel.cpp
@@ -57,7 +57,7 @@ void Channel::sendMessageToChannelUser(std::string msg, clientId cData, std::str
 	std::string toSend = ":" + cData._nickname + '!' + cData._username + "@tom " + cmd + " #" + _name + msg + "\r\n";
 	std::cout << "\033[36mSent in " << _name << "\033[0m :" << toSend;
 	for (std::map<std::string, clientInfo>::iterator it = _userMap.begin(); it != _userMap.end(); ++it) {
-		if (it->first != cData._nickname)
+		if (it->first != cData._nickname || cmd != "PRIVMSG")
 			send(it->second.clientFd, toSend.c_str(), toSend.size(), 0);
 	}
 }
